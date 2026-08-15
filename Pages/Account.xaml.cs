fabde06fabc152b74d5c2bf8d0179ec12ddd5b86
Adam Turner <aturner5278@gmail.com>
2026-08-15 05:38:11 +0100

    Create placeholder Account page

---
+++ Pages/Account.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TheatreDesktop.Pages
+{
+    /// <summary>
+    /// Interaction logic for Account.xaml
+    /// </summary>
+    public partial class Account : Page
+    {
+        public Account()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
