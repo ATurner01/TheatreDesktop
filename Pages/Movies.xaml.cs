01e2a940a5a328887737ef774e2aef47e9b1527a
Adam Turner <aturner5278@gmail.com>
2026-08-15 05:31:30 +0100

    Create placeholder movie page

---
+++ Pages/Movies.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TheatreDesktop
+{
+    /// <summary>
+    /// Interaction logic for Movies.xaml
+    /// </summary>
+    public partial class Movies : Page
+    {
+        public Movies()
+        {
+            InitializeComponent();
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
