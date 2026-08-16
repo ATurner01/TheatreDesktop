c7cff1f8879199476f6fafeb8f2545fb82248ddc
Adam Turner <aturner5278@gmail.com>
2026-08-16 21:53:04 +0100

    Implement CommandViewModel

---
+++ ViewModel/CommandViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Input;
+
+namespace TheatreDesktop.ViewModel
+{
+    class CommandViewModel : ViewModelBase
+    {
+        public ICommand Command { get; private set; }
+        public CommandViewModel(string displayName, ICommand command)
+        {
+            ArgumentNullException.ThrowIfNull(command);
+            base.DisplayName = displayName;
+            this.Command = command;
+        }
+    }
+}
