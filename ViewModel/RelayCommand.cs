30de4aacbc8952ebb60b91c2cf89acba0f7d0ff7
Adam Turner <aturner5278@gmail.com>
2026-08-16 22:24:47 +0100

    Implement RelayClass for simplified command injection

---
+++ ViewModel/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Input;
+
+namespace TheatreDesktop.ViewModel
+{
+    /// <summary>
+    /// A command that relays its functionality to other objects by invoking delegates. The default return value for the CanExecute method is 'true'.
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object?> _execute;
+        private readonly Predicate<object?> _canExecute;
+
+        public RelayCommand(Action<object?> execute) : this(execute, null) { }
+        public RelayCommand(Action<object?> execute, Predicate<object?> canExecute)
+        {
+            ArgumentNullException.ThrowIfNull(execute);
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
