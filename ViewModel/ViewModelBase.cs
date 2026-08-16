320b87a68832142f3cbd50c6c92669b52d9a7c6e
Adam Turner <aturner5278@gmail.com>
2026-08-16 21:44:21 +0100

    Implement base ViewModel class

---
+++ ViewModel/ViewModelBase.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace TheatreDesktop.ViewModel
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        string DisplayName { get; set; }
+        bool ThrowOnInvalidPropertyName { get; set; }
+        private bool disposed = false;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ViewModelBase(string  displayName)
+        {
+            DisplayName = displayName;
+            ThrowOnInvalidPropertyName = false;
+        }
+
+        public ViewModelBase() : this(string.Empty)
+        {
+        }
+
+        public void Dispose()
+        {
+            OnDispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void OnDispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    PropertyChanged = null;
+                }
+
+                this.disposed = true;
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            this.VerifyPropertyName(propertyName);
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+
+        public void VerifyPropertyName(string propertyName)
+        {
+            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+            {
+                string msg = "Invalid property name: " + propertyName;
+                if (ThrowOnInvalidPropertyName)
+                    throw new Exception(msg);
+                else
+                    System.Diagnostics.Debug.Fail(msg);
+            }
+        }
+    }
+}
