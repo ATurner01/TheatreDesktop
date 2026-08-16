8135d5fce74921afb6a6466c7b6f837b1d58559d
Adam Turner <aturner5278@gmail.com>
2026-08-16 22:31:20 +0100

    Implement PageViewModel abstract class

---
+++ ViewModel/PageViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace TheatreDesktop.ViewModel
+{
+    /// <summary>
+    /// Abstract class to represent a Page ViewModel in the MVVM pattern. Inherits from ViewModelBase and provides functionality for closing the page.
+    /// </summary>
+    abstract class PageViewModel : ViewModelBase
+    {
+        protected CommandViewModel? CloseCommand { get; set; }
+        public event EventHandler? CloseRequested;
+
+        public PageViewModel()
+        {
+            CloseCommand = new CommandViewModel("Close", new RelayCommand(param => this.OnRequestClose()));
+        }
+        public virtual void OnRequestClose()
+        {
+            CloseRequested?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
