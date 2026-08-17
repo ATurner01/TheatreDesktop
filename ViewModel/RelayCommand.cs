c054b42199986349ccc710a33045e430ee4316ef
Adam Turner <aturner5278@gmail.com>
2026-08-17 04:27:48 +0100

    Refactor RelayCommand class

--- ViewModel/RelayCommand.cs
+++ ViewModel/RelayCommand.cs
@@ -7,31 +7,32 @@ namespace TheatreDesktop.ViewModel
     /// </summary>
     public class RelayCommand : ICommand
     {
-        private readonly Action<object?> _execute;
-        private readonly Predicate<object?> _canExecute;
+        private readonly Action _execute;
+        private readonly Func<bool>? _canExecute;
 
-        public RelayCommand(Action<object?> execute) : this(execute, null) { }
-        public RelayCommand(Action<object?> execute, Predicate<object?> canExecute)
+        public RelayCommand(
+            Action execute,
+            Func<bool>? canExecute = null)
         {
-            ArgumentNullException.ThrowIfNull(execute);
-            _execute = execute;
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
         }
 
         public bool CanExecute(object? parameter)
         {
-            return _canExecute == null ? true : _canExecute(parameter);
-        }
-
-        public event EventHandler? CanExecuteChanged
-        {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
+            return _canExecute?.Invoke() ?? true;
         }
 
         public void Execute(object? parameter)
         {
-            _execute(parameter);
+            _execute();
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
         }
     }
 }
