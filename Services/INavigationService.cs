b31b956617920a315f61a079556cd10a83c5c1b7
Adam Turner <aturner5278@gmail.com>
2026-08-17 05:25:47 +0100

    Implement service to assist with page navigation

---
+++ Services/INavigationService.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace TheatreDesktop.Services
+{
+    public interface INavigationService
+    {
+        public void NavigateTo(string? pageKey);
+    }
+}
---
+++ Services/NavigationService.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Navigation;
+
+namespace TheatreDesktop.Services
+{
+    public class NavigationServiceHelper : INavigationService
+    {
+        private readonly DependencyObject _page;
+        public NavigationServiceHelper(DependencyObject page)
+        {
+            _page = page;
+        }
+
+        public void NavigateTo(string? pageKey)
+        {
+            ArgumentNullException.ThrowIfNull(pageKey, nameof(pageKey));
+            NavigationService nav = NavigationService.GetNavigationService(_page);
+            nav.Navigate(new Uri(pageKey, UriKind.Relative));
+        }
+    }
+}
