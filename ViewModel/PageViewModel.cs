65b8f66c686950116b7332fd1fa5632d5df0c0fc
Adam Turner <aturner5278@gmail.com>
2026-08-17 04:28:33 +0100

    Remove unnecessary code

--- ViewModel/PageViewModel.cs
+++ ViewModel/PageViewModel.cs
@@ -1,19 +1,15 @@
-﻿namespace TheatreDesktop.ViewModel
+﻿using System.Windows.Input;
+
+namespace TheatreDesktop.ViewModel
 {
     /// <summary>
     /// Abstract class to represent a Page ViewModel in the MVVM pattern. Inherits from ViewModelBase and provides functionality for closing the page.
     /// </summary>
     public abstract class PageViewModel : ViewModelBase
     {
-        public CommandViewModel? CloseCommand { get; set; }
-        public event EventHandler? RequestClose;
-
+        public ICommand? CloseCommand { get; set; }
         public PageViewModel()
         {
-        }
-        public virtual void OnRequestClose()
-        {
-            RequestClose?.Invoke(this, EventArgs.Empty);
         }
     }
 }
