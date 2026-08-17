0a7619145bccf19a679c87127f9fe0c3f0600a7d
Adam Turner <aturner5278@gmail.com>
2026-08-17 05:25:23 +0100

    Implement generic version of RelayCommand

--- ViewModel/RelayCommand.cs
+++ ViewModel/RelayCommand.cs
@@ -35,4 +35,37 @@ namespace TheatreDesktop.ViewModel
             CanExecuteChanged?.Invoke(this, EventArgs.Empty);
         }
     }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool>? _canExecute;
+        public RelayCommand(
+            Action<T> execute,
+            Func<T, bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+        public bool CanExecute(object? parameter)
+        {
+            if (parameter is T tParam)
+            {
+                return _canExecute?.Invoke(tParam) ?? true;
+            }
+            return false;
+        }
+        public void Execute(object? parameter)
+        {
+            if (parameter is T tParam)
+            {
+                _execute(tParam);
+            }
+        }
+        public event EventHandler? CanExecuteChanged;
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
 }
