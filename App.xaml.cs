e26f1bb0dea12feb25ed9ba38a25d11aa5fd5d59
Adam Turner <aturner5278@gmail.com>
2026-08-19 04:18:13 +0100

    Refactor App class to separate serivce setup into own class

--- App.xaml.cs
+++ App.xaml.cs
@@ -19,37 +19,8 @@ namespace TheatreDesktop
         {
             base.OnStartup(e);
 
-            var services = new ServiceCollection();
-
-            // Services
-            services.AddSingleton<IApplicationService, ApplicationService>();
-            services.AddSingleton<IDialogService, DialogService>();
-
-            // ViewModels
-            services.AddTransient<HomePageViewModel>();
-            services.AddTransient<MovieViewModel>();
-            services.AddTransient<AccountViewModel>();
-
-            // Views
-            services.AddTransient<HomePage>();
-            services.AddTransient<Movies>();
-            services.AddTransient<Account>();
-
-            // Main window
-            services.AddSingleton<MainWindow>();
-
-            // Navigation
-            services.AddSingleton<INavigationHost>(provider =>
-                provider.GetRequiredService<MainWindow>());
-
-            services.AddSingleton<INavigationService>(provider =>
-            {
-                var navigationHost = provider.GetRequiredService<INavigationHost>();
-
-                return new NavigationServiceHelper(
-                    navigationHost,
-                    provider);
-            });
+            var services = new ServiceCollection();         
+            ServiceHelper.RegisterServices(services);
 
             var _serviceProvider = services.BuildServiceProvider();
 
---
+++ Services/ServiceHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TheatreDesktop.ViewModel;
+using TheatreDesktop.Views;
+
+namespace TheatreDesktop.Services
+{
+    public class ServiceHelper
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Services
+            services.AddSingleton<IApplicationService, ApplicationService>();
+            services.AddSingleton<IDialogService, DialogService>();
+            // ViewModels
+            services.AddTransient<HomePageViewModel>();
+            services.AddTransient<MovieViewModel>();
+            services.AddTransient<AccountViewModel>();
+            // Views
+            services.AddTransient<HomePage>();
+            services.AddTransient<Movies>();
+            services.AddTransient<Account>();
+            // Main window
+            services.AddSingleton<MainWindow>();
+            // Navigation
+            services.AddSingleton<INavigationHost>(provider =>
+                provider.GetRequiredService<MainWindow>());
+            services.AddSingleton<INavigationService>(provider =>
+            {
+                var navigationHost = provider.GetRequiredService<INavigationHost>();
+                return new NavigationServiceHelper(
+                    navigationHost,
+                    provider);
+            });
+        }
+    }
+}
