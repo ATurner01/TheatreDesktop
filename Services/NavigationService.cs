7adf2889d2ec3ed579d374c6fb8764bf8999805a
Adam Turner <aturner5278@gmail.com>
2026-08-21 05:05:24 +0100

    Documentation

--- Services/NavigationService.cs
+++ Services/NavigationService.cs
@@ -5,11 +5,15 @@ using TheatreDesktop.Views;
 
 namespace TheatreDesktop.Services
 {
+    /// <summary>
+    /// A helper class that implements the INavigationService interface and provides navigation functionality for the application.
+    /// </summary>
     public class NavigationServiceHelper : INavigationService
     {
         private readonly INavigationHost _navigationHost;
         private readonly IServiceProvider _serviceProvider;
 
+        // A dictionary that maps ViewModel types to their corresponding Page types.
         private readonly Dictionary<Type, Type> _viewMappings = new()
         {
             { typeof(HomePageViewModel), typeof(HomePage) },
@@ -24,6 +28,10 @@ namespace TheatreDesktop.Services
             _serviceProvider = serviceProvider;
         }
 
+        /// <summary>
+        /// Navigates to the page associated with the specified ViewModel type.
+        /// </summary>
+        /// <typeparam name="TViewModel">The type of the ViewModel to navigate to.</typeparam>
         public void NavigateTo<TViewModel>()
         {
             var viewModel = _serviceProvider.GetService<TViewModel>();
@@ -32,6 +40,12 @@ namespace TheatreDesktop.Services
             _navigationHost.Navigate(page);
         }
 
+        /// <summary>
+        /// Navigates to the page associated with the specified ViewModel type, passing a parameter to the ViewModel's constructor.
+        /// </summary>
+        /// <typeparam name="TViewModel">The type of the ViewModel to navigate to.</typeparam>
+        /// <typeparam name="TParameter">The type of the parameter to pass to the ViewModel's constructor.</typeparam>
+        /// <param name="parameter">The parameter to pass to the ViewModel's constructor.</param>
         public void NavigateTo<TViewModel, TParameter>(TParameter parameter)
         {
             ArgumentNullException.ThrowIfNull(parameter, nameof(parameter));
@@ -44,6 +58,9 @@ namespace TheatreDesktop.Services
             _navigationHost.Navigate(page);
         }
 
+        /// <summary>
+        /// Navigates back to the previous page in the navigation stack.
+        /// </summary>
         public void GoBack()
         {
             _navigationHost.GoBack();
--- Services/RelayCommand.cs
+++ Services/RelayCommand.cs
@@ -3,13 +3,19 @@
 namespace TheatreDesktop.Services
 {
     /// <summary>
-    /// A command that relays its functionality to other objects by invoking delegates. The default return value for the CanExecute method is 'true'.
+    /// A class for creating a binding between WPF ICommand instances and C# methods. This allows us to use regular C# methods as commands in WPF controls.
     /// </summary>
     public class RelayCommand : ICommand
     {
         private readonly Action _execute;
         private readonly Func<bool>? _canExecute;
 
+        /// <summary>
+        /// Create a new instance of the RelayCommand class for executing the supplied Action. Takes an optional canExecute argument to determine if the Action should be run.
+        /// </summary>
+        /// <param name="execute">The Action that is to be executed.</param>
+        /// <param name="canExecute">An optional function delegate that determines if the Action supplied can be executed. Null by default.</param>
+        /// <exception cref="ArgumentNullException">Thrown in the event that null is provided as the value for the execute parameter.</exception>
         public RelayCommand(
             Action execute,
             Func<bool>? canExecute = null)
@@ -18,11 +24,20 @@ namespace TheatreDesktop.Services
             _canExecute = canExecute;
         }
 
+        /// <summary>
+        /// Invokes the canExecute delegate supplied on object creation, if defined. Returns true if the canExecute property is null.
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns>A conditional representing if the Action can be executed.</returns>
         public bool CanExecute(object? parameter)
         {
             return _canExecute?.Invoke() ?? true;
         }
 
+        /// <summary>
+        /// Executes the Action provided by the execute property.
+        /// </summary>
+        /// <param name="parameter"></param>
         public void Execute(object? parameter)
         {
             _execute();
@@ -30,6 +45,9 @@ namespace TheatreDesktop.Services
 
         public event EventHandler? CanExecuteChanged;
 
+        /// <summary>
+        /// Alerts that the canExecute property has been changed.
+        /// </summary>
         public void RaiseCanExecuteChanged()
         {
             CanExecuteChanged?.Invoke(this, EventArgs.Empty);
@@ -40,10 +58,12 @@ namespace TheatreDesktop.Services
     /// A modified version of RelayCommand that accepts a Generic Type parameter for passing arguments along with the specified Action.
     /// </summary>
     /// <typeparam name="T">The Type parameter which refers to the argument type of the Action.</typeparam>
+    /// <seealso cref="RelayCommand"/>
     public class RelayCommand<T> : ICommand
     {
         private readonly Action<T> _execute;
         private readonly Func<T, bool>? _canExecute;
+
         public RelayCommand(
             Action<T> execute,
             Func<T, bool>? canExecute = null)
@@ -51,6 +71,7 @@ namespace TheatreDesktop.Services
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
         }
+
         public bool CanExecute(object? parameter)
         {
             if (parameter is T tParam)
@@ -66,7 +87,9 @@ namespace TheatreDesktop.Services
                 _execute(tParam);
             }
         }
+
         public event EventHandler? CanExecuteChanged;
+
         public void RaiseCanExecuteChanged()
         {
             CanExecuteChanged?.Invoke(this, EventArgs.Empty);
